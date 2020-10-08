32fd86fec0d25504ac2c1e73f267295e724443a7
Charles <5richs@gmail.com>
2020-10-08 16:51:33 -0600

    Added a hello from Charles

--- HelloFromEthan/src/tindall/Main.java
+++ HelloFromEthan/src/tindall/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Ethan.");
+        System.out.println("Hello, Ethan. My name is Charles");
     }
 }
